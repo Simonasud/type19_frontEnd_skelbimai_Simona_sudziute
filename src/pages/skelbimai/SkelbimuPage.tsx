347a8e1952099ae6f01c74c22c6e588ade542ece
Simona Sud <ssimona.sud@gmail.com>
2024-04-02 14:11:19 +0300

    puslapiai

---
+++ src/pages/skelbimai/SkelbimuPage.tsx
@@ -0,0 +1,12 @@
+//
+
+function SkelbimuPage() {
+  return (
+    <div>
+      <h1>SkelbimuPage</h1>
+      <p>welcome to home page</p>
+    </div>
+  );
+}
+
+export default SkelbimuPage;
---
+++ src/pages/skelbimai/VienoSkelbimoPuslapis.tsx
@@ -0,0 +1,12 @@
+//
+
+function VienoSkelbimoPuslapis() {
+  return (
+    <div>
+      <h1>VienoSkelbimoPuslapis</h1>
+      <p>welcome to home page</p>
+    </div>
+  );
+}
+
+export default VienoSkelbimoPuslapis;
