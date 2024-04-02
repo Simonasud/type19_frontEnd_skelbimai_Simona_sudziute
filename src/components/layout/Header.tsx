b04794addcc30f0bd4f3c04b68bd12e8d56cbaca
Simona Sud <ssimona.sud@gmail.com>
2024-04-02 12:24:49 +0300

    sass

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,20 @@
+//
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <h2>logo</h2>
+        <nav>
+          <ul>
+            <li>
+              <a href=''>link</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
